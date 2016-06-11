15238ffe4bee029bb6ab1487686895970004c2de
Ravi Ranjan <stupidravi2009@gmail.com>
2016-06-11 23:56:44 +0530

    working fine on 6s plus but need to fix for other devices.

--- Alternative/ALTMainViewController.swift
+++ Alternative/ALTMainViewController.swift
@@ -9,72 +9,56 @@
 import UIKit
 
 class ALTMainViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var menuItemButton: UIBarButtonItem!
+    @IBOutlet weak var anythingElse: UIButton!
     
     let cellContents = [("food","Food Ordering"),("homeservices","Home Services"),("laundry","Dry Cleaning"),("cabs","Cab Booking"),("runner","Grocery Delivery"),("recharge","Richarge & Bills"),("gift","Gifts"),("cakeflowers","Cake & Flowers"),("movie","Movie TIckets")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Alternative"
+        anythingElse.layer.cornerRadius = 10.0
+        anythingElse.layer.borderWidth = 2.0
+        anythingElse.layer.borderColor = UIColor(red: 144/255, green: 38/255, blue: 25/255, alpha: 1.0).CGColor
         setStatusBarBackgroundColor(UIColor(red: 235/255, green: 161/255, blue: 14/255, alpha: 1.0))
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "homebackground")!)
         collectionView.backgroundColor = UIColor.clearColor()
         
         if revealViewController() != nil {
-            revealViewController().rearViewRevealWidth = 300
+            revealViewController().rearViewRevealWidth = 280
             menuItemButton.target = revealViewController()
-            menuItemButton.action = #selector(SWRevealViewController.revealToggle(_:))            
+            menuItemButton.action = #selector(SWRevealViewController.revealToggle(_:))
             view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-            
-            
         }
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    
     func setStatusBarBackgroundColor(color: UIColor) {
         
         guard  let statusBar = UIApplication.sharedApplication().valueForKey("statusBarWindow")?.valueForKey("statusBar") as? UIView else {
             return
         }
-        
         statusBar.backgroundColor = color
     }
     
     
     
-     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 1
     }
     
     
-     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return cellContents.count
     }
     
-     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! ALTMainCollectionViewCell
-        
         cell.cellImageView.image = UIImage(named: cellContents[indexPath.row].0)
         cell.cellLabel.text = cellContents[indexPath.row].1
-        
-           return cell
+        return cell
     }
-
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
